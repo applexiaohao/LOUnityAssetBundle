f23102c5acb115f61d76daba83a1174a5f1564ae
lewis <xiaohao@lanou3g.com>
2015-08-26 01:49:24 +0800

    AssetBundle

---
+++ LOUnityAsset/Assets/Editor/LOBuildAssetBundleEditor.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+
+/// <summary>
+/// 打包编辑器
+/// </summary>
+public class LOBuildAssetBundleEditor : EditorWindow {
+
+	/// <summary>
+	/// 获取不同平台下的包裹存储路径
+	/// </summary>
+	public static string GetAssetBundlePath(BuildTarget target)
+	{
+		string opp = "";
+
+		opp += Application.persistentDataPath;
+		opp += "AssetBundles/";
+
+		switch (target) {
+		case BuildTarget.iOS:
+			{
+				opp += "iOS/";
+				break;
+			}
+		case BuildTarget.StandaloneOSXUniversal:
+			{
+				opp += "MacOS/";
+				break;
+			}
+		default:
+			{
+				opp += "Others/";
+				break;
+			}
+		}
+
+		//当在硬盘目录结构里不存在该路径时,创建文件夹
+		if (!Directory.Exists(opp)) 
+		{
+			Directory.CreateDirectory (opp);
+		}
+
+		return opp;
+	}
+
+	[MenuItem("LOAssetBundles/Build")]
+	public static void CustomBuildAssetBundle()
+	{
+		//包裹存储的路径...
+		string outputPath = LOBuildAssetBundleEditor.GetAssetBundlePath(EditorUserBuildSettings.activeBuildTarget);
+
+		//输出路径
+		Debug.Log (outputPath);
+
+		//打包过程..
+		BuildPipeline.BuildAssetBundles(outputPath);
+	}
+}
---
+++ LOUnityAsset/Assets/Resources/Scripts/TestScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using LOAssetFramework;
+
+public class TestScript : MonoBehaviour {
+	
+	/// <summary>
+	/// 测试加载资源函数
+	/// </summary>
+	protected IEnumerator Load (string assetBundleName, string level)
+	{
+		IEnumerator b = da.LoadLevelAsync(assetBundleName, level, false);
+		yield return StartCoroutine(b);
+		Application.LoadLevel(level);
+	}
+
+	LOAssetManager da;
+	void Start () 
+	{
+		LOAssetManager.URI = "http://project.lanou3g.com/assetbundles/Others/";
+		LOAssetManager.ManifestName = "Others";
+		LOAssetManager.InitBlock = ((bool obj) => {
+			if (obj) {
+				StartCoroutine (Load ("scenes/loaderscene.unity3d", "LoaderScene"));
+			}
+		});
+
+		da = LOAssetManager.DefaultAssetManager;
+	}
+}
